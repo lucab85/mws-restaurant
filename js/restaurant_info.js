5bff33bd584cfc20af1608ecd3272e2daaac19b6
Luca Berton <mr.evolution85@gmail.com>
2018-03-08 12:21:35 +0100

    feat: Improved A11y
    Improved accessibility with introduction of tabindex and aria labels.
    
    Resolves: N/A
    See also: N/A

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -51,9 +51,11 @@ fetchRestaurantFromURL = (callback) => {
 fillRestaurantHTML = (restaurant = self.restaurant) => {
   const name = document.getElementById('restaurant-name');
   name.innerHTML = restaurant.name;
+  name.setAttribute("tabindex","0");
 
   const address = document.getElementById('restaurant-address');
   address.innerHTML = restaurant.address;
+  address.setAttribute("tabindex","0");
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
@@ -61,6 +63,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
+  cuisine.setAttribute("tabindex","0");
 
   // fill operating hours
   if (restaurant.operating_hours) {
@@ -80,13 +83,16 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
 
     const day = document.createElement('td');
     day.innerHTML = key;
+    day.setAttribute("tabindex","0");
     row.appendChild(day);
 
     const time = document.createElement('td');
     time.innerHTML = operatingHours[key];
+    time.setAttribute("tabindex","0");
     row.appendChild(time);
 
     hours.appendChild(row);
+    hours.setAttribute("tabindex","0");
   }
 }
 
@@ -97,11 +103,13 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
   const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
+  title.setAttribute("tabindex","0")
   container.appendChild(title);
 
   if (!reviews) {
     const noReviews = document.createElement('p');
     noReviews.innerHTML = 'No reviews yet!';
+    noReviews.setAttribute("tabindex","0");
     container.appendChild(noReviews);
     return;
   }
@@ -119,18 +127,22 @@ createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
   name.innerHTML = review.name;
+  name.setAttribute("tabindex","0");
   li.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
+  date.setAttribute("tabindex","0");
   li.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.setAttribute("tabindex","0");
   li.appendChild(rating);
 
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
+  comments.setAttribute("tabindex","0");
   li.appendChild(comments);
 
   return li;
